4a66a3f4c7e86fe32e33936bc7c4ff6231d2394a
Alexandre Soares <amsoares@gmail.com>
2021-04-18 03:00:37 +0200

    added support for ubuntu

--- scripts/linux/update-system.sh
+++ scripts/linux/update-system.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-yum update -y
+# Get OS type and update it
+if cat /etc/os-release | grep -w ID | grep centos; then
+    OS="centos"
+    echo "[INFO] CentOS detected..."
+    # Update all
+    yum update -y
+    # Install software
+    yum install -y \
+        net-tools \
+        wget
 
-# Install software
-yum install -y \
-    net-tools \
-    wget
+elif cat /etc/os-release | grep -w ID | grep ubuntu; then
+    OS="ubuntu"
+    echo "[INFO] Ubuntu detected..."
+    # Update all
+    apt update
+    apt upgrade -y
+    # Install software
+    apt install net-tools
+
+else
+    echo "[ERROR] OS not supported..."
+fi
